c5042268c4becf38e98f91f0c619e33607d0477e
Fikir Alemayehu <fikiralex243@gmail.com>
2023-03-17 15:32:34 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,12 +3,11 @@
  * main - print single base 10 digit starting from 0
  * Description: using the main function
  * this program prints "0123456789"
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int d;
-	
 	for (d = 0 ; d < 10 ; d++)
 	{
 		printf("%d", d);
