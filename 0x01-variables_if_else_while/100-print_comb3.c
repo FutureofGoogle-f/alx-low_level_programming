7f3ecc0aef8c8fa84abc705d6548236e9e357b8c
Fikir Alemayehu <fikiralex243@gmail.com>
2023-03-17 16:24:21 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - print all possible different combinations of two digits 
+ * Description: using the main function
+ * this program prints "01,02,03,04,05,06,07,08,09.."
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+	for (i = 0; i <= 9 ; i++)
+	{
+		for (j = 1 ; j <= 9 ; j++)
+		{
+			if (j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
