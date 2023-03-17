330929bdb965a337d2b17546cb7b09a218396e46
Fikir Alemayehu <fikiralex243@gmail.com>
2023-03-17 15:06:49 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this program prints "abcdefghijklmnopqrstuvwxyz"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
