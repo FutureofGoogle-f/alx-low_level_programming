c161953c503a2a0bb0ab7480b3ee227fab4b3640
Fikir Alemayehu <fikiralex243@gmail.com>
2023-03-21 16:34:23 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alpabet_x10 - prints 10 times the alphabet
+ * print_alpabet_x10 - a function that prints 10 times the alphabet
  * Description: using print_alpabet_x10 function
  * Return: x10 a-z
  */
