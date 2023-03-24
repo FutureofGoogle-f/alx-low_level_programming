f5cdf185ce06d83299892831c5ee92c34a12a964
Fikir Alemayehu <fikiralex243@gmail.com>
2023-03-23 18:11:27 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar(45);
-		m = n *=1;
+		m = n *= 1;
 	}
 	else
 	{
@@ -22,9 +22,9 @@ void print_number(int n)
 	while (d > 9)
 	{
 		d /= 10;
-		count *=10;
+		count *= 10;
 	}
-	for (; count >= 1; count /=10)
+	for (; count >= 1; count /= 10)
 	{
 		_putchar(((m / count) % 10) + 48);
 	}
