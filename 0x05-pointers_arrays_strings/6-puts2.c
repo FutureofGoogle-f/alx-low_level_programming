cea2bfd9e3d05401ae58d3b737c138e4de728156
Fikir Alemayehu <fikiralex243@gmail.com>
2023-03-27 18:21:22 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - prints one char out of two strings
+ * @str: the string containing characters
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+	for (index = 0; index < len; += 2)
+		_putchar(str[index]);
+	_putchar('\n');
+}
