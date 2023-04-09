57bbcb5383905684a176e6971c803c825fbdb8df
Fikir Alemayehu <fikiralex243@gmail.com>
2023-04-09 10:40:17 -0700

    task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Description: 'the program's description'
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
